8fac7d77b94d4618d9341ef3a7618b6766d275af
Alexa275 <alexam022003@gmail.com>
2025-01-03 18:48:03 +0000

    et575

---
+++ day2_Jan3/lab3_Martinez.cpp
@@ -0,0 +1,24 @@
+/*
+Alexa Martinez
+Jan3, numerical variables
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\v------- Example 1: integer variables ------ "<<endl;
+    //declare identifier, variable's name
+    int height;
+    int width;
+    int area;
+    cout<<"Initial value area = "<<area<<endl;
+    // assign values to each variable
+    height = 5;
+    width = 2;
+    area = height*width;
+    cout<<"calculated area = "<<area<<endl;
+
+
+    return 0;
+}
